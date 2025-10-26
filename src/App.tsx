64066a80951a5b43eec81acd4da9c393b4e7c70e
Zhiguang Chen <zhiguang@hanlogy.com>
2025-10-26 06:17:25 +0100

    disable some log

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@
  */
 
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, LogBox} from 'react-native';
 
 import {styles} from './App.styles';
 import {RootContextProvider} from './states/RootContext';
+
+LogBox.ignoreLogs([
+  'Sending `onAnimatedValueUpdate` with no listeners registered.',
+]);
 
 const App = (): React.JSX.Element => {
   return (
