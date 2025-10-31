4b60d712d94ab2544d973403536c408734f77afe
Zhiguang Chen <zhiguang@hanlogy.com>
2025-10-31 09:10:39 +0100

    delete resolveHeaders

--- __tests__/lib/parseWorkspace/resolveRecordSource.test.ts
+++ __tests__/lib/parseWorkspace/resolveRecordSource.test.ts
@@ -3,10 +3,20 @@ import {resolveRecordSource} from '@/lib/parseWorkspace/resolveRecordSource';
 describe('resolveRecordSource', () => {
   test('return empty', () => {
     expect(resolveRecordSource()).toStrictEqual({});
+    expect(resolveRecordSource({source: undefined} as any)).toStrictEqual({});
     expect(resolveRecordSource({source: null} as any)).toStrictEqual({});
     expect(resolveRecordSource({source: true} as any)).toStrictEqual({});
+    expect(resolveRecordSource({source: false} as any)).toStrictEqual({});
     expect(resolveRecordSource({source: 10} as any)).toStrictEqual({});
     expect(resolveRecordSource({source: {}})).toStrictEqual({});
+  });
+
+  test('ignore undefined', () => {
+    const result = resolveRecordSource({
+      source: {name: 'foo', b: undefined} as any,
+    });
+
+    expect(result).toEqual({name: 'foo'});
   });
 
   test('result a string record', () => {
