9fa692e4cb067187d6a24f7c95554a714431aa04
Zhiguang Chen <zhiguang@hanlogy.com>
2025-11-26 19:48:23 +0100

    Update mock server page.implement HttpServer

--- src/components/MockServersView/MockServersView.styles.ts
+++ src/components/MockServersView/MockServersView.styles.ts
@@ -35,6 +35,72 @@ export function createStyles({ isLight, colors }: Theme) {
         borderLeftColor: isLight ? '#EDEDED' : '#333',
         borderLeftWidth: 1,
       },
+
+      serverConfig: {
+        padding: 16,
+      },
+    }),
+
+    serverContext: StyleSheet.create({
+      container: {
+        display: 'flex',
+      },
+
+      toolbar: {
+        height: 50,
+        backgroundColor: isLight ? '#FFF' : '#333',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 16,
+      },
+
+      toggleButton: {
+        backgroundColor: '#DDD',
+        paddingHorizontal: 16,
+        height: 36,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderRadius: 18,
+      },
+      toggleButtonHovered: {
+        backgroundColor: '#EEE',
+      },
+      toggleButtonPressed: {
+        backgroundColor: '#DDD',
+      },
+    }),
+
+    serverItemStyles: StyleSheet.create({
+      button: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        height: 32,
+        borderRadius: 16,
+      },
+      buttonHovered: {
+        backgroundColor: isLight ? '#EEE' : '#333',
+      },
+      buttonPressed: {
+        backgroundColor: isLight ? '#DDD' : '#2F2F2F',
+      },
+      routesList: {
+        paddingLeft: 16,
+      },
+
+      statusDot: {
+        width: 12,
+        height: 12,
+        backgroundColor: isLight ? '#CCC' : '#999',
+        borderRadius: 6,
+        marginRight: 8,
+      },
+      statusDotActive: {
+        backgroundColor: '#74a216ff',
+      },
     }),
   };
 }
--- src/components/MockServersView/MockServersView.tsx
+++ src/components/MockServersView/MockServersView.tsx
@@ -2,15 +2,41 @@ import { useThemeContext } from '@/states/theme';
 import { useWorkspaceContext } from '@/states/workspace';
 import { Text, ScrollView, View } from 'react-native-macos';
 import { createStyles } from './MockServersView.styles';
+import { Clickable } from '../clickables';
+import { useCallback, useMemo, useState } from 'react';
+import type { MockServer } from '@/definitions';
+import { JsonViewer } from '../JsonViewer';
 
 export function MockServersView() {
-  const { workspace } = useWorkspaceContext();
+  const { workspace, isServerRunning, startServer, stopServer } =
+    useWorkspaceContext();
   const { theme } = useThemeContext();
-  const { styles } = createStyles(theme);
+  const { styles, serverItemStyles, serverContext } = createStyles(theme);
+  const [currentServer, setCurrentServer] = useState<MockServer>();
+
+  const isRunning = useMemo(() => {
+    if (!currentServer?.port || !isServerRunning) {
+      return false;
+    }
+    return isServerRunning(currentServer.port);
+  }, [currentServer?.port, isServerRunning]);
+
+  const toggleServer = useCallback(() => {
+    if (!startServer || !stopServer || !currentServer?.port) {
+      return;
+    }
+    if (isRunning) {
+      stopServer(currentServer.port);
+    } else {
+      startServer(currentServer.port);
+    }
+  }, [stopServer, startServer, currentServer?.port, isRunning]);
 
   if (!workspace) {
     return <></>;
   }
+
+  const { servers } = workspace;
 
   return (
     <View style={styles.container}>
@@ -20,11 +46,50 @@ export function MockServersView() {
         </View>
 
         <ScrollView contentContainerStyle={styles.leftBarContent}>
-          <Text>Server list</Text>
+          {servers.map(server => {
+            const { name, port } = server;
+            return (
+              <Clickable
+                key={[name, port].join('_')}
+                onPress={() => {
+                  setCurrentServer(server);
+                }}
+                style={serverItemStyles.button}
+                hoveredStyle={serverItemStyles.buttonHovered}
+                pressedStyle={serverItemStyles.buttonPressed}>
+                <View
+                  style={[
+                    serverItemStyles.statusDot,
+                    isRunning ? serverItemStyles.statusDotActive : undefined,
+                  ]}
+                />
+                <Text>{name}</Text>
+              </Clickable>
+            );
+          })}
         </ScrollView>
       </View>
       <View style={styles.mainContent}>
-        <Text>TODO: Route config</Text>
+        {currentServer ? (
+          <View style={serverContext.container}>
+            <View style={serverContext.toolbar}>
+              <Clickable
+                onPress={() => toggleServer()}
+                style={serverContext.toggleButton}
+                hoveredStyle={serverContext.toggleButtonHovered}
+                pressedStyle={serverContext.toggleButtonPressed}>
+                <Text>{isRunning ? 'Stop' : 'Start'}</Text>
+              </Clickable>
+            </View>
+            <ScrollView contentContainerStyle={styles.serverConfig}>
+              <JsonViewer value={currentServer} />
+            </ScrollView>
+          </View>
+        ) : (
+          <View>
+            <Text />
+          </View>
+        )}
       </View>
     </View>
   );
