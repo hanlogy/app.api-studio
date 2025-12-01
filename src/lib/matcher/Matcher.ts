680b4ab403e018dd77c54f5d77a313e30c3ff0ba
Zhiguang Chen <zhiguang@hanlogy.com>
2025-12-01 10:11:00 +0100

    Create Matcher interface

---
+++ src/lib/matcher/Matcher.ts
@@ -0,0 +1,21 @@
+// Asymmetric matcher interface and helpers
+
+import { isPlainObject } from '@/helpers/checkTypes';
+
+// Brand
+export const MATCHER = Symbol('mock.matcher');
+
+export interface Matcher {
+  readonly $$typeof: typeof MATCHER;
+  readonly name: string;
+  test(actual: unknown): boolean;
+}
+
+export function isMatcher(x: unknown): x is Matcher {
+  return (
+    isPlainObject(x) &&
+    x.$$typeof === MATCHER &&
+    typeof x.test === 'function' &&
+    typeof x.name === 'string'
+  );
+}
